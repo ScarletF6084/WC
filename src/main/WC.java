17bcf0c7d87bb0c8c7c903661f4530760ce45c16
ScarletF6084 <ScarletF6084@users.noreply.github.com>
2020-03-23 15:15:56 +0800

    WC

--- src/main/WC.java
+++ src/main/WC.java
@@ -13,23 +13,30 @@ public class WC {
 
             System.out.println("请输入指令。");
             String command = in.next();
-            char flag = '0';
+            char[] flag = new char[5];
 
             if (!command.equals("wc.exe")) {
                 System.out.println("输入有误，请先输入 wc.exe");
             }
+
+            int i = 0;
             command = in.next();
-            switch (command){
-                case "-c":flag = 'c';break;
-                case "-w":flag = 'w';break;
-                case "-l":flag = 'l';break;
-                case "-a":flag = 'a';break;
-                case "exit": System.exit(0);
-                default: {
-                    System.out.println("输入有误，可用命令为：-c, -w, -l, -a, exit");
+            do {
+                switch (command){
+                    case "-c":flag[i] = 'c';break;
+                    case "-w":flag[i] = 'w';break;
+                    case "-l":flag[i] = 'l';break;
+                    case "-a":flag[i] = 'a';break;
+                    case "exit": System.exit(0);
+                    default: {
+                        System.out.println("输入有误，可用命令为：-c, -w, -l, -a, exit");
+                    }
                 }
-            }
-            path = in.next();
+                i++;
+                command = in.next();
+            }while (command.toCharArray()[0] == '-');
+
+            path = command;
             File f = new File(path);
             //判断输入的文件是否合法
             if(!f.exists()){
@@ -42,19 +49,23 @@ public class WC {
                 System.out.println("文件无法读取。");
             }
             else{
-                Counter wc = new Counter(path);
-                switch (flag){
-                    case 'c': wc.printChars();break;
-                    case 'w': wc.printWords();break;
-                    case 'l': wc.printLines();break;
-                    case 'a':{
-                        wc.printCodeLines();
-                        wc.printCommentLines();
-                        wc.printBlankLines();
-                        break;
+                int temp = 0;
+                while(i-- > 0){
+                    Counter wc = new Counter(path);
+                    switch (flag[temp]){
+                        case 'c': wc.printChars();break;
+                        case 'w': wc.printWords();break;
+                        case 'l': wc.printLines();break;
+                        case 'a':{
+                            wc.printCodeLines();
+                            wc.printCommentLines();
+                            wc.printBlankLines();
+                            break;
+                        }
+                        default:
+                            System.out.println("失败。");
                     }
-                    default:
-                        System.out.println("失败。");
+                    temp++;
                 }
             }
         }
